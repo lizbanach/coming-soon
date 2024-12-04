5a1e0cb22674b2efb79692efb4315a211dd48713
Liz Banach <erbanach@gmail.com>
2024-12-04 13:36:56 -0800

    feat: coming soon landing page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: 'var(--blue)',
+        mustard: 'var(--mustard)',
+        black: 'var(--black)',
+        linen: 'var(--linen)',
+        red: 'var(--red)',
+        peach: 'var(--peach)',
+      },
+      fontFamily: {
+        body: ['Crimson Pro, ui-serif'],
       },
     },
   },
